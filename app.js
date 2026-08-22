8cce3d3c3e2345a73fa5e1df8a53ec2e888d1353
Ayb <aymojobe@gmail.com>
2026-08-22 17:18:54 +0100

    morgan, cors, and bodyparser setup

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const express = require("express");
+const morgan = require ("morgan");
+
 require('dotenv/config');
 const env = process.env;
 const app = express();
 
+app.use(bodyParser.json())
+app.use(morgan);
+app.use(cors());
+app.options('/', cors());
+
 app.listen(env.PORT,env.HOSTNAME,() => {
      console.log(`Server ru at http://${env.HOSTNAME}:${env.PORT}`);
 })
-
-
-app.use((req,res,next) => {
-console.log("A a request has been made to your sever user");
-return next();
-});
-app.use((req,res,next) => {
-console.log("A a request has been made to your sever again user 2");
-return next();
-});
-
-
-const authorization = (req,res,next) => {
-     const isAuthorized = true;
-     if(isAuthorized) {
-          console.log("user is authorized");
-          return next();
-     }else {
-          return res.status(401).send("you shall not pass user");
-     }
-};
-
-app.get("/useryou/yoyo/:id", authorization, (req ,res) => {
+app.get("/useryou/yoyo/:id", (req ,res) => {
      return res.json(`there ya go ya little user ${req.params.id}` );
 })
 app.get("/", (req ,res) => {
