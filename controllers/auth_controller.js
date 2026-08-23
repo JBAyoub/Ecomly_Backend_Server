6a1b4dfb70c5a484a439d26370b3409fdf3f5315
Ayb <aymojobe@gmail.com>
2026-08-24 00:51:04 +0100

    User registration

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,8 +1,40 @@
+const {validationResult} = require('express-validator');
+const { User } = require('../models/user');
+const { default: bcrypt } = require('bcryptjs');
+
+
 exports.login = async function (req,res)  {
      return res.status(200).send("User logged in successfully");
 }
+
 exports.register = async function (req,res)  {
-     return res.status(200).send("User register  successfully");
+     const errors= validationResult(req);
+     if (!errors.isEmpty()){
+          const errorMessages = errors.array().map((error) => ({
+               field: error.path,
+               message: error.msg
+          }));
+         
+          res.status(400).json({
+               errors: errorMessages
+          });
+     }
+try {
+     let user = new User({
+          ...req.body,
+          passwordHash: bcrypt.hashSync(req.body.password,8)
+     })
+     user = await user.save();
+     if (!user){
+          return res.status(500).json({
+               type:"Internal Server Error",
+               Message:"Could not create user"        
+          });
+     }
+     return res.status(200).json(user);
+} catch (error) {
+     return res.status(500).json({type:error.name, message: error.message});
+}
 }
 exports.forgotPassword = async function (req,res)  {
      return res.status(200).send("User forgot-password successfully");
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const authRouter = require('express').Router();
 const authController = require('../controllers/auth_controller');
-
-authRouter.post("/login", authController.login );
-authRouter.post("/register", authController.login);
+const {body} = require('express-validator');
+const validateUser = [
+     body('name').notEmpty().withMessage('Name is required'),
+     body('email').isEmail().withMessage('Please enter a valid email'),
+     body('password').isLength({min:8}).withMessage('Password must be atleast 8 characters')
+     .isStrongPassword().withMessage('Password must contain atleast 1 Uppercase, 1 symbol'),
+     body('phone').isMobilePhone().withMessage('Please enter a valid phone number')
+]
+authRouter.post("/login",authController.login );
+authRouter.post("/register", validateUser, authController.register);
 authRouter.get("/forgot-password", authController.forgotPassword);
 authRouter.get("/verify-otp", authController.verifyOtp);
 authRouter.get("/reset-password",authController.resetPassword);
