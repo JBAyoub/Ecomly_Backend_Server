75212b7b63856aed68acabb9344e30614e0d3e3a
Ayb <aymojobe@gmail.com>
2026-08-25 18:58:44 +0100

    added response in case checks didnt check

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -45,6 +45,7 @@ async function errorHandler(error, req, res, next) {
                     .json({ type: 'Unauthorized', message: refreshError.message })
           }
      }
+     return res.status(404).json({ type: error.name, message: error.message })
 }
 
 module.exports = errorHandler;
