481d86b26de540c1445e453fb6bb5aa069fc97cb
Ayb <aymojobe@gmail.com>
2026-08-28 16:28:33 +0100

    review remodel

--- models/review.js
+++ models/review.js
@@ -1,11 +1,39 @@
 const { Schema, model } = require('mongoose');
 
 const reviewSchema = Schema({
-     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-     userName: { type: String, required: true },
-     comment: { type: String, trim: true },
-     rating: { type: Number, required: true },
-     date: { type: Date, default: Date.now },
+     product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true
+     },
+
+     user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true
+     },
+
+     userName: {
+          type: String,
+          required: true
+     },
+
+     comment: {
+          type: String,
+          trim: true
+     },
+
+     rating: {
+          type: Number,
+          required: true,
+          min: 1,
+          max: 5
+     },
+
+     date: {
+          type: Date,
+          default: Date.now
+     }
 });
 
 reviewSchema.set('toJSON', { virtuals: true });
